3f1ef9f99122e40402deb8166e782ce64b6e3e78
johntalent <talent.corp@gmail.com>
2015-04-03 23:57:16 -0300

    Tag with Closure support

--- grails-app/controllers/sustenagro/AdminController.groovy
+++ grails-app/controllers/sustenagro/AdminController.groovy
@@ -8,7 +8,6 @@ import com.hp.hpl.jena.query.QueryFactory
 import com.hp.hpl.jena.query.ResultSet
 import com.hp.hpl.jena.rdf.model.ModelFactory
 import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP
-import groovy.json.JsonSlurper
 
 class Dsl {
 
@@ -17,7 +16,7 @@ class Dsl {
     def missing     = [:]
     def properties
 
-    //def elems       = [ 'a', 'p', 'b', 'i', 'mark', 'del', 's', 'ins', 'u', 'small', 'strong', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'abbr', 'footer', 'cite', 'dl', 'dt', 'dd', 'code', 'pre', 'var', 'samp', 'span']
+    def elems       = [ 'a', 'p', 'b', 'i', 'mark', 'del', 's', 'ins', 'u', 'small', 'strong', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'abbr', 'footer', 'cite', 'dl', 'dt', 'dd', 'code', 'pre', 'var', 'samp', 'span']
     def containers  = [ 'div', 'blockquote', 'address', 'form' ]                    //containers
     def lists       = [ 'ul', 'ol', 'li']                                           //lists
     def tables      = [ 'table', 'thead', 'tbody', 'tr', 'th', 'td']                //tables
@@ -26,7 +25,7 @@ class Dsl {
 
     def currentParent   = null
  
-    public Dsl(elems){
+    public Dsl(){
         elems.each({baseFunction(it)})
         containers.each({baseFunction(it)})
         lists.each({baseFunction(it)})
@@ -35,7 +34,7 @@ class Dsl {
         imgs.each({baseFunction(it)})
     }
 
-    def baseFunction = { it ->
+    def oldFunction = { it ->
         Dsl.metaClass."$it" = { Map map = [:], content ->
             
             if(content.metaClass.respondsTo(content, "call")){
@@ -60,7 +59,40 @@ class Dsl {
         }
     }
 
-    void properties(Map map) { println "properties"}
+    def baseFunction = { it ->
+        Dsl.metaClass."$it" = { Closure cl ->
+            def tag       = new Tag(this)
+            def code      = cl.rehydrate(tag, this, this)
+            code.resolveStrategy = Closure.DELEGATE_ONLY
+
+            print 'base Delegate'
+            println code.getDelegate()
+            print 'base Owner'
+            println code.getOwner()
+            print 'base ThisObject'
+            println code.getThisObject() 
+            print 'base getDirective'
+            println code.getDirective()
+
+            //def node      = markup.createNode(it, tag.properties, tag.content)
+            def node      = markup.createNode(it)
+            this.currentParent = node
+
+            def text      = code()
+            println markup.getMkp()
+
+            if (text != null ){
+                markup.getMkp().yield(text)
+            }
+            else{
+                println tag.properties.getClass()
+                markup.getMkp().yield(tag.content)
+            }
+            
+            this.currentParent = null
+            markup.nodeCompleted(this.currentParent, node)
+        }
+    }
 
     def make(Closure closure) {
         println "\nStart of the analyze "
@@ -76,6 +108,52 @@ class Dsl {
     }
 }
 
+class Tag {
+
+    Map properties
+    String content
+    Dsl currentParent
+
+    public Tag(){
+        this.content = ''
+        this.properties = [:]
+        println "construct basic"
+    }
+
+    public Tag(Dsl parent){
+        this.content = ''
+        this.properties = [:]
+        currentParent = parent
+    }
+
+    public content( content ){
+        if(content.metaClass.respondsTo(content, "call")){
+            def tag = new Tag(this.currentParent)
+            def code = content.rehydrate(this.currentParent, this, this)
+            code.resolveStrategy = Closure.DELEGATE_ONLY
+            
+            print 'closure Delegate'
+            println code.getDelegate()
+            print 'closure Owner'
+            println code.getOwner()
+            print 'closure ThisObject'
+            println code.getThisObject()
+            print 'closure getDirective'
+            println code.getDirective()
+
+            code()
+        }
+        else if (content.class.simpleName == "String"){
+            this.content = content
+            println "String method"
+        }
+    }
+
+    public properties( Map properties ){
+        this.properties = properties
+    }
+}
+
 class AdminController {
 
     def index(){
@@ -85,7 +163,7 @@ class AdminController {
     def elems = []
 
     def AdminController(){
-        String queryStr =   "PREFIX ui_ontology: <http://bio.icmc.usp.br:8888/sustenagro/ui_ontology#>" +
+        /*String queryStr =   "PREFIX ui_ontology: <http://bio.icmc.usp.br:8888/sustenagro/ui_ontology#>" +
                             "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
                                 "select distinct ?name where {" +
                                     "?s rdf:type ui_ontology:Tag ." +
@@ -101,7 +179,7 @@ class AdminController {
 
         while(rs.hasNext()){
             elems += rs.next().get("name")
-        }
+        }*/
     }
 
     def GroovyArchitecture(){
@@ -110,7 +188,7 @@ class AdminController {
         //Dsl dsl = new Dsl(elems)
         //binding.setVariable("dsl", dsl)
         
-        GroovyShell shell = new GroovyShell(new Binding([dsl: new Dsl(elems)]))
+        GroovyShell shell = new GroovyShell(new Binding([dsl: new Dsl()]))
 
         //Script script = shell.parse("dsl.make({" + params["code"] + "})")
         //render script.run()
@@ -130,85 +208,85 @@ class AdminController {
 }
 
 //"Language prototype = 
-        /*
+/*
 
-        h1 "bootstrap"
-        
-        mark "bootstrap"
-        
-        del "bootstrap"
-        
-        small "hola mundo"
-        
-        p 'class': 'lead', "bootstrap"
-        
-        div 'class':'container-fluid', { p 'class': 'lead', "bootstrap" }
+h1 "bootstrap"
 
-        blockquote { p { "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." } }
-        
-        ul {
-            li{"1"}
-            li{"2"}
-        }
-        
-        div 'class': 'well', { p { 'bootstrap' }  }
-        
-        div 'class': 'well',  {
-            div 'class': 'well', {
-                p 'class': 'lead', {
-                    "hola mundo"
-                }
-            }
-        }
-                
-        ol 'class': "c1", {
-            li 'class': "l1", {1}
-            li 'class': "l2", {2}
-            li 'class': "l2", {3}
-            li 'class': "l2", {4}
-            li 'class': "l2", {5}
-            li 'class': "l2", {6}
-        }
-        
-        table 'class':"table table-hover", {
-            thead{
-                tr{
-                  th{'#'}
-                  th{'First Name'}
-                  th{'Last Name'}
-                  th{'Username'}
-                }
-            }
-            tbody{
-                tr{
-                  th 'scope':"row", {'1'}
-                  td{ 'Mark' }
-                  td{ 'Otto' }
-                  td{ '@mdo' }
-                }
-            }
-        }
+mark "bootstrap"
 
-        div 'class':"form-group", {
-            label 'for':"exampleInputEmail1", { 'Email address' }
-            input 'type': "email", 'class':"form-control", 'id':"exampleInputEmail1['hola']", 'placeholder':"Enter email", {}
-        }
+del "bootstrap"
 
-        input 'type': "email", 'class':"form-control", 'id':"exampleInputEmail", {}
+small "hola mundo"
 
-        div 'class':"row", {
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
-          div 'class':"col-md-1",{".col-md-1"}
+p 'class': 'lead', "bootstrap"
+
+div 'class':'container-fluid', { p 'class': 'lead', "bootstrap" }
+
+blockquote { p { "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante." } }
+
+ul {
+    li{"1"}
+    li{"2"}
+}
+
+div 'class': 'well', { p { 'bootstrap' }  }
+
+div 'class': 'well',  {
+    div 'class': 'well', {
+        p 'class': 'lead', {
+            "hola mundo"
         }
+    }
+}
         
-        */
+ol 'class': "c1", {
+    li 'class': "l1", {1}
+    li 'class': "l2", {2}
+    li 'class': "l2", {3}
+    li 'class': "l2", {4}
+    li 'class': "l2", {5}
+    li 'class': "l2", {6}
+}
+
+table 'class':"table table-hover", {
+    thead{
+        tr{
+          th{'#'}
+          th{'First Name'}
+          th{'Last Name'}
+          th{'Username'}
+        }
+    }
+    tbody{
+        tr{
+          th 'scope':"row", {'1'}
+          td{ 'Mark' }
+          td{ 'Otto' }
+          td{ '@mdo' }
+        }
+    }
+}
+
+div 'class':"form-group", {
+    label 'for':"exampleInputEmail1", { 'Email address' }
+    input 'type': "email", 'class':"form-control", 'id':"exampleInputEmail1['hola']", 'placeholder':"Enter email", {}
+}
+
+input 'type': "email", 'class':"form-control", 'id':"exampleInputEmail", {}
+
+div 'class':"row", {
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+  div 'class':"col-md-1",{".col-md-1"}
+}
+
+*/
