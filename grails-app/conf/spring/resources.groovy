582c649da5553e7a680cf421af9542752e476e95
Dilvan Moreira <dilvan@gmail.com>
2015-07-08 18:24:00 -0300

    New slurper.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,8 +1,10 @@
+import rdfSlurper.RDFSlurper
 import sustenagro.MemStore
 import sustenagro.SlurpRDF
 
 // Place your Spring DSL code here
 beans = {
     g(MemStore)
-    s(SlurpRDF,g)
+    s(SlurpRDF, g)
+    slp(RDFSlurper, 'ontology/SustenAgroOntology.rdf')
 }
--- grails-app/controllers/sustenagro/ToolController.groovy
+++ grails-app/controllers/sustenagro/ToolController.groovy
@@ -1,10 +1,12 @@
 package sustenagro
 
 import com.github.slugify.Slugify
+import static rdfSlurper.RDFSlurper.N
 
 class ToolController {
     def g
-    def s
+    //def s
+    def slp
 
     def index() {
         def microregions = []
@@ -12,12 +14,19 @@ class ToolController {
         def technologies = []
         def production_unit_types = []
 
-        println s.'dbpedia:MicroRegion'
+        //println slp.isa('dbp:MicroRegion').where
 
-        g.v('dbpedia:MicroRegion').in("rdf:type").each{ microregions.push(id: it.id, name: it.out('sustenagro:name').next().value) }
-        g.v('sustenagro:SugarcaneProductionSystem').in("rdf:type").each{ cultures.push(id: it.id, name: it.out('sustenagro:name').next().value) }
-        g.v('sustenagro:AgriculturalTechnology').in("rdf:type").each{ technologies.push(id: it.id, name: it.out('sustenagro:name').next().value, 'description': it.out('sustenagro:description').next().value) }
-        g.v('sustenagro:ProductionUnit').in("rdfs:subClassOf").each{ production_unit_types.push(id: it.id, name: it.out('rdfs:label').has('lang','pt').next().value) }
+        microregions = slp.sparql('select * where {?id a dbp:MicroRegion; sa:name ?name.}')
+
+        //slp.'dbp:MicroRegion'.in('rdf:type').each{ microregions.push(id: it.id, name: it.out('sa:name').next().value) }
+
+        cultures = slp.sparql('select * where {?id a sa:SugarcaneProductionSystem; sa:name ?name.}')
+        technologies = slp.sparql('select * where {?id a sa:AgriculturalTechnology; sa:name ?name; sa:description ?description.}')
+        production_unit_types = slp.sparql("select * where {?id rdfs:subClassOf sa:ProductionUnit; rdfs:label ?name. filter (lang(?name) = 'pt')}")
+
+        //slp.'sa:SugarcaneProductionSystem'._().in("rdf:type").each{ cultures.push(id: it.id, name: it.out('sa:name').next().value) }
+        //slp.'sa:AgriculturalTechnology'._().in("rdf:type").each{ technologies.push(id: it.id, name: it.out('sa:name').next().value, 'description': it.out('sa:description').next().value) }
+        //slp.'sa:ProductionUnit'._().in("rdfs:subClassOf").each{ production_unit_types.push(id: it.id, name: it.out('rdfs:label').has('lang','pt').next().value) }
 
         render(view: "index", model:    [microregions: microregions,
                                          cultures: cultures,
@@ -28,7 +37,17 @@ class ToolController {
     def productionUnitCreate() {
         Slugify slug = new Slugify()
 
+        //def g = slp.g
+
         def production_unit_id = slug.slugify(params["production_unit_name"])
+//        N('sa:'+production_unit_id,
+//                'rdf:type': slp[params["production_unit_type"]],
+//                'sa:name': params["production_unit_name"],
+//                'sa:microregion': slp[params["production_unit_microregion"]],
+//                'sa:culture': slp[params["production_unit_culture"]],
+//                'sa:technology': slp[params["production_unit_technology"]]
+//        )
+
         def v_production_unit = g.addVertex("sustenagro:"+production_unit_id) //Add Microregion to id
 
         g.addEdge(v_production_unit, g.v(params["production_unit_type"]), 'rdf:type')
@@ -49,8 +68,28 @@ class ToolController {
         def social_indicators = []
         def categorical = [:]
 
-        //g.v('sustenagro:EnvironmentalIndicator').in("rdfs:subClassOf").each{ environmental_indicators.push(  id: it.id, title: it.out('terms:title').has('lang','pt').next().value, description: it.out('terms:description').has('lang','pt').next().value, assessmentQuestion: it.out('sustenagro:assessmentQuestion').has('lang','pt').next().value) }
-        g.v('sustenagro:EnvironmentalIndicator').in("rdfs:subClassOf").in("rdfs:subClassOf").each{ environmental_indicators.push(id: it.id, title: it.out('rdfs:label').has('lang','pt').next().value, 'class': it.out("rdfs:subClassOf").has("kind","bnode").out("owl:onClass").next().id, 'valueType': it.out("rdfs:subClassOf").has("kind","bnode").out("owl:onClass").out("rdfs:subClassOf").next().id) }
+        //def g = slp.g
+
+//        g.v('sustenagro:EnvironmentalIndicator').in("rdfs:subClassOf").each{ environmental_indicators.push(  id: it.id, title: it.out('terms:title').has('lang','pt').next().value, description: it.out('terms:description').has('lang','pt').next().value, assessmentQuestion: it.out('sustenagro:assessmentQuestion').has('lang','pt').next().value) }
+        g.v('sustenagro:EnvironmentalIndicator').in("rdfs:subClassOf").in("rdfs:subClassOf").each{
+            environmental_indicators.push(
+                    id: it.id,
+                    title: it.out('rdfs:label').has('lang','pt').next().value,
+                    'class': it.out("rdfs:subClassOf").has("kind","bnode").out("owl:onClass").next().id,
+                    'valueType': it.out("rdfs:subClassOf").has("kind","bnode").out("owl:onClass").out("rdfs:subClassOf").next().id)
+        }
+//        slp.sparql('?id rdfs:subClassOf ?a. ?a rdfs:subClassOf sa:EnvironmentalIndicator.') each {
+//            environmental_indicators.push(
+//                    id:
+//            )
+//        }
+//        environmental_indicators = slp.sparql(
+//                'select * where {' +
+//                        '?id rdfs:subClassOf ?a. ' +
+//                        '?a  rdfs:subClassOf sa:EnvironmentalIndicator. ' +
+//                        '?id rdfs:label ?title. filter (lang(?title) = "pt")}')
+//
+
         g.v('sustenagro:EconomicIndicator').in("rdfs:subClassOf").in("rdfs:subClassOf").each{ economic_indicators.push(id: it.id, title: it.out('rdfs:label').has('lang','pt').next().value, 'class': it.out("rdfs:subClassOf").has("kind","bnode").out("owl:onClass").next().id, 'valueType': it.out("rdfs:subClassOf").has("kind","bnode").out("owl:onClass").out("rdfs:subClassOf").next().id) }
         g.v('sustenagro:SocialIndicator').in("rdfs:subClassOf").in("rdfs:subClassOf").each{ social_indicators.push(id: it.id, title: it.out('rdfs:label').has('lang','pt').next().value, 'class': it.out("rdfs:subClassOf").has("kind","bnode").out("owl:onClass").next().id, 'valueType': it.out("rdfs:subClassOf").has("kind","bnode").out("owl:onClass").out("rdfs:subClassOf").next().id) }
 
